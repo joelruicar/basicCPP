30eee4c513af018e6ede93fed058fcd808c307b5
Joel <79532707+joelruicar@users.noreply.github.com>
2024-01-18 02:18:15 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  // Definir las variables
+  int numero1 = 2;
+  int numero2 = 2;
+
+  // Realizar la suma
+  int resultado = numero1 + numero2;
+
+  // Imprimir el resultado en pantalla
+  std::cout << "El resultado de la suma 2 + 2 es: " << resultado << std::endl;
+
+  return 0;
+}
